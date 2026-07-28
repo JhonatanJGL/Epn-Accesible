80bcb49b7c55c0410f6d09e6beed171d48101358
narishama <jhonatanguacapina@gmail.com>
2026-07-28 17:56:01 -0500

    Configurar despliegue automático con Firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,23 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCChYOr1g5V2RhkwIYNmrirFs35at8WJV4",
   authDomain: "accesibilidad-1d3c0.firebaseapp.com",
   projectId: "accesibilidad-1d3c0",
   storageBucket: "accesibilidad-1d3c0.firebasestorage.app",
   messagingSenderId: "611703827062",
-  appId: "1:611703827062:web:97b3d571524036f6c14d57"
+  appId: "1:611703827062:web:97b3d571524036f6c14d57",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
 export default app;
